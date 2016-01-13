fc71dcccf302d551dbabc10d4b812fcea2b880bb
xdjlao <jerry@flatie.com>
2016-01-13 13:13:47 -0800

    initial commit

---
+++ Twitterizer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Twitterizer
+//
+//  Created by Jerry on 1/13/16.
+//  Copyright © 2016 Jerry Lao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Twitterizer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Twitterizer
+//
+//  Created by Jerry on 1/13/16.
+//  Copyright © 2016 Jerry Lao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
